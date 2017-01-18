94a155c00303648dcf82f8cfc0489cdaff27bb64
Joel Martinez <joelmartinez@gmail.com>
2017-01-18 13:53:04 -0500

    initial checkin

---
+++ EcmaUrlParser.Sample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Monodoc.Ecma;
+
+namespace EcmaUrlParserSample
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var parser = new EcmaUrlParser();
+			string someMethod = "M:MyNamespace.MyType.MyMethod<T,K>(string,MyOtherNamespace.OtherType)";
+
+			EcmaDesc desc;
+			parser.TryParse(someMethod, out desc);
+
+			Console.WriteLine($"namespace {desc.Namespace}, name {desc.MemberName}, number of arguments {desc.MemberArgumentsCount}");
+
+			foreach (var genericParameter in desc.GenericMemberArguments)
+				Console.WriteLine($"generic parameter: {genericParameter.TypeName}");
+
+
+			foreach (var parameter in desc.MemberArguments)
+				Console.WriteLine($"namespace: {parameter.Namespace}, name: {parameter.TypeName}");
+		}
+	}
+}
